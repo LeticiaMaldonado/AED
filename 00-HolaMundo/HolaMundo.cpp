034772b40b831ccb1bf680311e7f382eb3e0fdc5
LeticiaMaldonado <49348626+LeticiaMaldonado@users.noreply.github.com>
2019-04-08 01:15:16 -0300

    Update HolaMundo.cpp

--- 00-HolaMundo/HolaMundo.cpp
+++ 00-HolaMundo/HolaMundo.cpp
@@ -1,1 +1,14 @@
+/*
+	AED - TP0 - Hola Mundo
+	Programa que Genera la salida de una Cadena "Hola Mundo :)"
+	Leticia Maldonado - K1051 - Legajo 163.708-3
+	20190406
+*/
 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hola Mundo :)\n";
+}
