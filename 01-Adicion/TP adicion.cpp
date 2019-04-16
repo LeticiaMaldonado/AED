62eb6bfa5668f2ce02c070ef09f753be55104085
LeticiaMaldonado <49348626+LeticiaMaldonado@users.noreply.github.com>
2019-04-16 19:33:32 -0300

    Add files via upload

---
+++ 01-Adicion/TP adicion.cpp
@@ -0,0 +1,20 @@
+/* 
+TP 1: Adicion
+Descripcion: Sumar dos numeros poporcionados por el usuario y mostrarle la suma
+Autor: Leticia Maldonado
+Fecha de ultima modificacion: 16/04/2019
+*/
+
+
+# include <iostream>
+
+using namespace std;
+
+main () {
+
+int a=0; int b=0;
+cout<< "Ingrese un numero: "; cin>>a;
+cout<< "\nIngrese otro numero: "; cin>>b;
+cout<< "\nEl resultado de la adicion es: " << a+b;
+}
+
